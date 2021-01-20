59eb916b40e79c9e471bb485993fd297a9a07c42
BR88C <bcroethel88@gmail.com>
2021-01-20 16:25:21 -0500

    temp

--- src/commands/templates/_dirconfigtemplate.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: ``,
-    fullName: ``,
-    aliases: [],
-    description: ``,
-    ignore: false
-}
--- src/utils/randomInt.js
+++
@@ -1,9 +0,0 @@
-/* Generates a random integer between min and max */
-
-const randomInt = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-module.exports = randomInt;
