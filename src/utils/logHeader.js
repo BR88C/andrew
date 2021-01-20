04a16d8d6ef300f582a55a0dd21edd5dc5edd70c
BR88C <bcroethel88@gmail.com>
2021-01-20 16:23:24 -0500

    Update logHeader.js

--- src/utils/logHeader.js
+++ src/utils/logHeader.js
@@ -3,12 +3,12 @@
 const config = require(`../config/config.js`);
 
 const logHeader = () => {
-    console.log(`\x1b[34m`, `______    _            _ `);
-    console.log(`\x1b[34m`, `| ___ \\  | |          | |`);
-    console.log(`\x1b[34m`, `| |_/ /__| |_ ___ _ __| |`);
-    console.log(`\x1b[34m`, `|  __/ _ \\ __/ _ \\ '__| |`);
-    console.log(`\x1b[34m`, `| | |  __/ ||  __/ |  |_|`);
-    console.log(`\x1b[34m`, `\\_|  \\___|\\__\\___|_|  (_)`);
+    console.log(`\x1b[34m`, `  ___            _                   _ `);
+    console.log(`\x1b[34m`, ` / _ \\          | |                 | |`);
+    console.log(`\x1b[34m`, `/ /_\\ \\_ __   __| |_ __ _____      _| |`);
+    console.log(`\x1b[34m`, `|  _  | '_ \\ / _\` | '__/ _ \\ \\ /\\ / / |`);
+    console.log(`\x1b[34m`, `| | | | | | | (_| | | |  __/\\ V  V /|_|`);
+    console.log(`\x1b[34m`, `\\_| |_/_| |_|\\__,_|_|  \\___| \\_/\\_/ (_)`);
     console.log(`\x1b[34m`, `\n By ${config.devs.tags.join(`, `)}\n`);
 };
 
