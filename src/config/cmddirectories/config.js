77454fee2db48f89379d9cae1cf3655ff9d3c2cc
BR88C <bcroethel88@gmail.com>
2021-01-20 16:44:33 -0500

    add config category

---
+++ src/config/cmddirectories/config.js
@@ -0,0 +1,9 @@
+const configDir = {
+    name: `config`,
+    fullName: `Bot Config`,
+    aliases: [`bot configuration`, `configuration`, `botconfig`],
+    description: `Commands for configuring Andrew!`,
+    ignore: false
+};
+
+module.exports = configDir;
