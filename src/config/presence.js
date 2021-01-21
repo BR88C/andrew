ea235cae8b823864f2a1a15867f3e79d9c5f43b9
BR88C <bcroethel88@gmail.com>
2021-01-20 19:39:54 -0500

    Update presence.js

--- src/config/presence.js
+++ src/config/presence.js
@@ -1,7 +1,14 @@
 const presence = [{
         activity: {
-            name: `a kid's guide to the Internet`,
-            type: `WATCHING`
+            name: `a!help`,
+            type: `PLAYING`
+        },
+        status: `online`
+    },
+    {
+        activity: {
+            name: `a!setup`,
+            type: `PLAYING`
         },
         status: `online`
     },
@@ -9,6 +16,13 @@ const presence = [{
         activity: {
             name: `on the internet`,
             type: `PLAYING`
+        },
+        status: `online`
+    },
+    {
+        activity: {
+            name: `a kid's guide to the Internet`,
+            type: `WATCHING`
         },
         status: `online`
     }
