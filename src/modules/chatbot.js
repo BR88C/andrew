0f48d64e078788dc3b1c76c3a38a0ee3021fa2fc
BR88C <bcroethel88@gmail.com>
2021-01-21 13:29:46 -0500

    add chatbot type 1

--- src/modules/chatbot.js
+++ src/modules/chatbot.js
@@ -1,25 +1,47 @@
 /* Function to send a request and get a response from the chatbot api */
 
 const fetch = require(`node-fetch`);
+const Markov = require(`markov-strings`).default
+const markovData = require(`../config/markovData.js`);
 const config = require(`../config/config.js`);
 const log = require(`../modules/log.js`);
+const randomInt = require(`../utils/randomInt.js`);
 
 let chatbot = async (message, type) => {
-    console.log(type)
+    if (type === 0) {
+        let requestURL = `https://api.udit.gq/api/chatbot?message=${encodeURIComponent(message)}&name=${config.chatbot.name}&gender=${config.chatbot.gender}`;
 
-    let requestURL = `https://api.udit.gq/api/chatbot?message=${encodeURIComponent(message)}&name=${config.chatbot.name}&gender=${config.chatbot.gender}`;
+        let res = await fetch(requestURL).catch(error => {
+            log(error, `red`);
+            return `An unknown error occured.`
+        });
 
-    let res = await fetch(requestURL).catch(error => {
-        log(error, `red`);
-        return `An unknown error occured.`
-    });
+        let resJSON = await res.json().catch(error => {
+            log(error, `red`);
+            return `An unknown error occured.`
+        });
 
-    let resJSON = await res.json().catch(error => {
-        log(error, `red`);
-        return `An unknown error occured.`
-    });
+        return resJSON.message.length === 0 ? `An unknown error occured.` : resJSON.message
+    } else if (type === 1) {
+        let trainingData = markovData
+        trainingData.push(message.content);
 
-    return resJSON.message.length === 0 ? `An unknown error occured.` : resJSON.message
+        let markov = new Markov({
+            stateSize: 2
+        })
+
+        markov.addData(trainingData);
+
+        let options = {
+            maxTries: 15,
+            prng: Math.random,
+        };
+
+
+        let res = markov.generate(options);
+
+        return res.string.length === 0 ? `An unknown error occured.` : res.string;
+    }
 };
 
 module.exports = chatbot;
